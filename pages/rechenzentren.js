7f86ae080991c0270eed1c9b592bb29217a20dac
sanoj765 <jonas.graze@outlook.com>
2022-06-24 11:11:01 +0200

    Adjusted desing of back button and did the correct linking

--- pages/rechenzentren.js
+++ pages/rechenzentren.js
@@ -1,4 +1,5 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
 
@@ -6,9 +7,11 @@ export default function Home() {
         <div>
             <div className={styles.site}>
                 <h1 className={styles.title}>
-                    <div className={styles.backButton}>
-                        <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
-                    </div>
+                    <Link href="/">
+                        <div className={styles.backButton}>
+                            <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                        </div>
+                    </Link>
                     Rechenzentren
                 </h1>
                 <h2>
