91fea8ee58df41478eecd94ad7aea6ec53810812
sanoj765 <jonas.graze@outlook.com>
2022-06-24 11:14:02 +0200

    Linking for Netzwerke and Endgeraete

--- pages/endgeraete.js
+++ pages/endgeraete.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
 
@@ -7,6 +8,11 @@ export default function Home() {
         <div>
             <div className={styles.site}>
                 <h1 className={styles.title}>
+                    <Link href="/">
+                        <div className={styles.backButton}>
+                            <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                        </div>
+                    </Link>
                     Endgeräte
                 </h1>
                 <h2>
