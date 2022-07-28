6e7cb4b287ab13cf8eb77b8074c9c86b13297888
marvinmuenger <marvin.muenger@uzh.ch>
2022-07-28 14:32:13 +0200

    make footer stick to bottom

--- pages/faq.js
+++ pages/faq.js
@@ -54,27 +54,28 @@ const config = {
 export default function Home() {
 
     return (
-
-        <div>
+        <div className={styles.spaceFooter}>
             <Header />
             <div className={styles.site}>
-                <h1 className={styles.title}>
-                    <Link href="/">
-                        <div className={styles.backButton}>
-                            <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
-                        </div>
-                    </Link>
-                    FAQ
-                </h1>
-                <h2>
-                    Hier gibt es Antworten zu häufig gestellten Fragen.
-                </h2>
-                <div style={{marginTop: "-2rem", marginLeft: "0.5rem", width: "95%"}}>
-                    <Faq
-                        data={data}
-                        styles={faqstyles}
-                        config={config}
-                    />
+                <div className={styles.content}>
+                    <h1 className={styles.title}>
+                        <Link href="/">
+                            <div className={styles.backButton}>
+                                <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                            </div>
+                        </Link>
+                        FAQ
+                    </h1>
+                    <h2>
+                        Hier gibt es Antworten zu häufig gestellten Fragen.
+                    </h2>
+                    <div style={{marginTop: "-2rem", marginLeft: "0.5rem", width: "95%"}}>
+                        <Faq
+                            data={data}
+                            styles={faqstyles}
+                            config={config}
+                        />
+                    </div>
                 </div>
             </div>
             <Footer />
--- pages/netzwerke.js
+++ pages/netzwerke.js
@@ -7,27 +7,29 @@ import Footer from "../components/footer";
 export default function Home() {
 
     return (
-        <div>
+        <div className={styles.spaceFooter}>
             <Header />
             <div className={styles.site}>
-                <h1 className={styles.title}>
-                    <Link href="/">
-                        <div className={styles.backButton}>
-                            <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
-                        </div>
-                    </Link>
-                    Netzwerke
-                </h1>
-                <h2>
-                    Hier erfährst du mehr über die Netzwerke.
-                </h2>
-                <main>
-                    <card className={styles.textBoxPages}>
-                        <p className={styles.cardTextPages}>
-                            Die Kommunikationsnetze verbrauchen bei der Übertragung der gewünschten Videodatei an den Nutzer des Streamingangebots Energie. Entscheidend für die Höhe des Energieverbrauch sind hier die Grösse der Videodatei und die Effizienz, wie diese übertragen wird. Dateien werden entweder über Kabel oder über Funkmasten übertragen. Generell verbraucht dabei die Übertragung über Mobilfunk mehr Energie. Glasfaserkabel und 5G sind ihren Vorgängern Kupferkabel, 3G und 4G klar vorzuziehen und ein Ausbau dieser Infrastruktur würde den Energieverbrauch reduzieren. Um die Grösse der Videodatei zu reduzieren und damit den Energieverbrauch zu senken, kann der Nutzer eine geringere Auflösung der gewünschten Videodatei wählen. Ausserdem ermöglichen Komprimierungsstandards immer effektivere Möglichkeiten, die Grösse der Videodateien zu reduzieren.
-                        </p>
-                    </card>
-                </main>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>
+                        <Link href="/">
+                            <div className={styles.backButton}>
+                                <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                            </div>
+                        </Link>
+                        Netzwerke
+                    </h1>
+                    <h2>
+                        Hier erfährst du mehr über die Netzwerke.
+                    </h2>
+                    <main>
+                        <card className={styles.textBoxPages}>
+                            <p className={styles.cardTextPages}>
+                                Die Kommunikationsnetze verbrauchen bei der Übertragung der gewünschten Videodatei an den Nutzer des Streamingangebots Energie. Entscheidend für die Höhe des Energieverbrauch sind hier die Grösse der Videodatei und die Effizienz, wie diese übertragen wird. Dateien werden entweder über Kabel oder über Funkmasten übertragen. Generell verbraucht dabei die Übertragung über Mobilfunk mehr Energie. Glasfaserkabel und 5G sind ihren Vorgängern Kupferkabel, 3G und 4G klar vorzuziehen und ein Ausbau dieser Infrastruktur würde den Energieverbrauch reduzieren. Um die Grösse der Videodatei zu reduzieren und damit den Energieverbrauch zu senken, kann der Nutzer eine geringere Auflösung der gewünschten Videodatei wählen. Ausserdem ermöglichen Komprimierungsstandards immer effektivere Möglichkeiten, die Grösse der Videodateien zu reduzieren.
+                            </p>
+                        </card>
+                    </main>
+                </div>
             </div>
             <Footer />
         </div>
