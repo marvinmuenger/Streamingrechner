9e8e122225d6d9cd8be5cc0ca318951b1c28ca4d
marvinmuenger <marvin.muenger@uzh.ch>
2022-07-28 14:37:40 +0200

    add passhref

--- pages/faq.js
+++ pages/faq.js
@@ -59,7 +59,7 @@ export default function Home() {
             <div className={styles.site}>
                 <div className={styles.content}>
                     <h1 className={styles.title}>
-                        <Link href="/">
+                        <Link href="/" passHref>
                             <div className={styles.backButton}>
                                 <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
                             </div>
