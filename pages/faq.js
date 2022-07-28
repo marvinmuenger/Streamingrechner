f787921d957f2adf41580b4d105ec81b11190e58
marvinmuenger <marvin.muenger@uzh.ch>
2022-07-28 13:08:21 +0200

    add header and footer to faq

--- pages/faq.js
+++ pages/faq.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Faq from "react-faq-component";
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const data = {
     title: "FAQ",
@@ -54,27 +56,28 @@ export default function Home() {
     return (
 
         <div>
-        <div className={styles.site}>
-            <h1 className={styles.title}>
-                <Link href="/">
-                    <div className={styles.backButton}>
-                        <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
-                    </div>
-                </Link>
-                FAQ
-            </h1>
-            <h2>
-                Hier gibt es Antworten zu häufig gestellten Fragen.
-            </h2>
-            <div style={{marginTop: "-2rem", marginLeft: "0.5rem", width: "95%"}}>
-                <Faq
-                    data={data}
-                    styles={faqstyles}
-                    config={config}
-                />
+            <Header />
+            <div className={styles.site}>
+                <h1 className={styles.title}>
+                    <Link href="/">
+                        <div className={styles.backButton}>
+                            <svg className={styles.backArrow} xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                        </div>
+                    </Link>
+                    FAQ
+                </h1>
+                <h2>
+                    Hier gibt es Antworten zu häufig gestellten Fragen.
+                </h2>
+                <div style={{marginTop: "-2rem", marginLeft: "0.5rem", width: "95%"}}>
+                    <Faq
+                        data={data}
+                        styles={faqstyles}
+                        config={config}
+                    />
+                </div>
             </div>
+            <Footer />
         </div>
-
-    </div>
     );
 }
