923ef5a5213784f6ab86ae49605c3f545c0f58ec
marvinmuenger <marvin.muenger@uzh.ch>
2022-07-30 11:02:37 +0200

    change head title

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import '../styles/globals.css'
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../theme/theme.js'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Streaming-Rechner</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   )
 }
 
