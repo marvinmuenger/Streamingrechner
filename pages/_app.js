8cb001c378d56f95ba083e238553c0cc93be78d2
marvinmuenger <marvin.muenger@uzh.ch>
2022-07-30 10:43:46 +0200

    change favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,12 @@
 import '../styles/globals.css'
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../theme/theme.js'
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
